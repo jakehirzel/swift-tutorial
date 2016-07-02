c5cf0d1c7080a5a376f665e2d6a7780e56e65bc2
jakehirzel <hirzel@hotmail.com>
2016-07-02 08:44:04 -0400

    Finished tutorial. Successful run in Simulator (quitting and re-launching), but SIGABRT on 	quitting and relaunching the Simulator.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -19,8 +19,14 @@ class MealTableViewController: UITableViewController {
         // Allow for edit button
         navigationItem.leftBarButtonItem = editButtonItem()
         
-        // Load sample data
-        loadSampleMeals()
+        // Load saved meals or else load sample data
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }
+        else {
+            // Load sample data
+            loadSampleMeals()
+        }
     }
     
     func loadSampleMeals() {
@@ -77,10 +83,11 @@ class MealTableViewController: UITableViewController {
         if editingStyle == .Delete {
             // Delete the row from the data source
             meals.removeAtIndex(indexPath.row)
+            saveMeals()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 
     /*
@@ -131,6 +138,19 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
             }
+            saveMeals()
         }
+    }
+    
+    // MARK: NSCoding
+    func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print("Failed to save meal list.")
+        }
+    }
+
+    func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as? [Meal]
     }
 }
